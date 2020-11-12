23a629de4fa4fd1a7c9542d2d2bb31d3da9e3d2f
Hansart Marine <hansart.marine@gmail.com4>
2020-11-12 10:00:02 +0100

    db

--- public/js/scriptCalendarPreview.js
+++ public/js/scriptCalendarPreview.js
@@ -7,18 +7,37 @@ style.sheet.insertRule(`body {height: ${h}px}`);
 $('body').prepend(`<div class="overlay h-100 w-100"></div>`);
 $('.overlay').css({'display':'none'});
 
-// modifier le css du container en foncction du layout
+
+// modifier le css du container en fonction du layout
 if($('.layout-carre')){
-    console.log("hello");
     $("#calendar-container").css({
       display: "flex",
       "flex-wrap": "wrap",
     });
   }
-
+  var today = new Date();
+  var mois    = today.getMonth() + 1;
+  var jour    = today.getDate();
+  today = jour + '/' + mois 
+  var currentDay;
     // ajouter la porte à chaque case
     $(".day").each(function (j) {
-        $(this).prepend(`<div class="porte">${j+1}</div>`);
+      var dayDate = j+1;
+        $(this).prepend(`<div class="porte">${dayDate}</div>`);
+        
+        if(((j+1).toString()).length === 1 ){
+          currentDay = `0${j+1}`
+        }else{
+          currentDay=(j+1).toString();
+        }
+        // if( mois == 12 ){
+        //   console.log(mois);
+        //   if( jour < currentDay){
+        //     $(this).css({'pointer-events':'none'})
+        //   }
+        // }else{
+        //   $(this).css({'pointer-events':'none'})
+        // }
       })
 
         //au clic, actions !
@@ -37,3 +56,4 @@ if($('.layout-carre')){
           })
             $(this).children('.porte').addClass('open');
           });
+
